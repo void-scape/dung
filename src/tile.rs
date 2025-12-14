649917ab7f74f095fd81aa2cacffa773b1917fd7
green-ogre <balln13572@gmail.com>
2025-12-13 19:43:06 -0700

    Tile components

--- src/tile.rs
+++ src/tile.rs
@@ -6,6 +6,66 @@ pub fn plugin(app: &mut App) {
         .add_systems(Update, tile_sprite);
 }
 
+/// Marks a tile as obstructive to entity movement.
+#[derive(Default, Component)]
+pub struct Solid;
+
+#[derive(Component)]
+#[require(TileSprite::WALL, Solid)]
+pub struct Wall;
+
+#[derive(Component)]
+#[require(TileSprite::FLOOR)]
+pub struct Floor;
+
+#[derive(Component)]
+pub struct TileSprite {
+    pub ascii: u8,
+    pub fg: Color,
+    pub bg: Color,
+}
+
+impl TileSprite {
+    pub const WALL: Self = Self {
+        ascii: b'x',
+        fg: Color::WHITE,
+        bg: Color::BLACK,
+    };
+
+    pub const FLOOR: Self = Self {
+        ascii: b'.',
+        fg: Color::WHITE,
+        bg: Color::BLACK,
+    };
+}
+
+fn tile_sprite(
+    mut commands: Commands,
+    atlas: Res<TileAtlas>,
+    tiles: Query<(Entity, &TileSprite), Added<TileSprite>>,
+) {
+    for (entity, tile) in tiles.iter() {
+        commands.entity(entity).insert((
+            Sprite {
+                image: atlas.image.clone(),
+                texture_atlas: Some(TextureAtlas {
+                    layout: atlas.atlas.clone(),
+                    index: tile.ascii as usize,
+                }),
+                color: tile.fg,
+                ..Default::default()
+            },
+            Anchor::BOTTOM_LEFT,
+        ));
+        commands.spawn((
+            Transform::from_xyz(0.0, 0.0, -1.0),
+            Sprite::from_color(tile.bg, Vec2::splat(TILE_SIZE as f32)),
+            Anchor::BOTTOM_LEFT,
+            ChildOf(entity),
+        ));
+    }
+}
+
 #[derive(Resource)]
 pub struct TileAtlas {
     pub image: Handle<Image>,
@@ -27,42 +87,4 @@ fn spawn_atlas(
             None,
         )),
     });
-}
-
-#[derive(Component)]
-pub struct TileSprite {
-    pub ascii: u8,
-    pub fg: Color,
-    pub bg: Color,
-}
-
-#[derive(Component)]
-pub struct TileBg;
-
-fn tile_sprite(
-    mut commands: Commands,
-    atlas: Res<TileAtlas>,
-    tiles: Query<(Entity, &TileSprite), Added<TileSprite>>,
-) {
-    for (entity, tile) in tiles.iter() {
-        commands.entity(entity).insert((
-            Sprite {
-                image: atlas.image.clone(),
-                texture_atlas: Some(TextureAtlas {
-                    layout: atlas.atlas.clone(),
-                    index: tile.ascii as usize,
-                }),
-                color: tile.fg,
-                ..Default::default()
-            },
-            Anchor::BOTTOM_LEFT,
-        ));
-        commands.spawn((
-            TileBg,
-            Transform::from_xyz(0.0, 0.0, -1.0),
-            Sprite::from_color(tile.bg, Vec2::splat(TILE_SIZE as f32)),
-            Anchor::BOTTOM_LEFT,
-            ChildOf(entity),
-        ));
-    }
 }
